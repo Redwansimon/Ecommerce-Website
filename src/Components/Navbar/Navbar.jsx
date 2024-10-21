f109d7d307c5b51afdf53bccc88d26ce6b055526
final git <redwan.pust@gmail.com>
2024-10-21 20:59:50 +0600

    nav name change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt="" />
-            <p>SHOPPER</p>
+            <p>WELL-<span style={{color:'purple'}}>MART</span></p>
         </div>
         <ul className="nav-menu">
             <li onClick={()=>{setmenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link> {menu ==="shop"?<hr/>:<></>}</li>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ecommerce-Website/'  // Update this to match your GitHub repository name
 })
+
